48172c2d9d5d617ef23a7601886a51b85c372b9b
Yun William Yu <ywyu@math.toronto.edu>
2022-01-22 14:03:36 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i><span class="usernames">' + socket.username + '</span> joined the chat..</i>');
+        console.log(socket.handshake.headers["x-real-ip"] + ': ' + socket.username + " joined");
+    });
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', message.name + ": " + message.message);
+        console.log(socket.handshake.headers["x-real-ip"] + ': ' + message.name + ": " + message.message.replace(/(\r\n|\r|\n)/g, '\\n'));
+    });
+
+});
+
+const server = http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
